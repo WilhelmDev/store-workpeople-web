6469c8965089b7e9db9328fa5f68d5658e8dd521
Wilhelm Flores <wiljoseflores@gmail.com>
2025-03-29 23:02:50 -0400

    feat: add new interface for cart

---
+++ src/interfaces/cart/index.ts
@@ -0,0 +1,15 @@
+import { Product } from '../product'
+
+export interface CartItem {
+  id: number
+  cartId: number
+  productId: number
+  quantity: number
+  product: Product
+}
+
+export interface Cart {
+  id: number
+  userId: number
+  items: CartItem[]
+}
